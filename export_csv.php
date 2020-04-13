e907de446c12ff43738f0bf6dc4f2355eb31fa5b
JPG <jpg.soporte@gmail.com>
2020-04-13 09:09:25 -0400

    fix utf8 encoding for csv export

--- export_csv.php
+++ export_csv.php
@@ -2,9 +2,11 @@
 include 'connection.php';
 include 'index_controller.php';
 //dd($products);
-header('Content-Type: application/csv; charset=UTF-8');
+header('Content-Encoding: UTF-8');
+header('Content-type: text/csv; charset=UTF-8');
 header('Content-Disposition: attachment; filename="'.'quarantine_stock_'.date('d-m-Y').'.csv'.'";');
 ob_clean();
+echo "\xEF\xBB\xBF"; // UTF-8 BOM
 $fp = fopen('php://output', 'w');
 //$fp = fopen('quarantine_stock_'.date('d-m-Y').'.csv', 'w');
 fputcsv($fp, $fields,';');
